b87c364853290f8f4caf4562a2c566ab4d850530
polesskiy <polesskiy.dev@gmail.com>
2016-07-08 18:13:26 +0300

    Add docs to version control

---
+++ public/doc/api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "wm-task-tracker",
+  "version": "0.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-07-08T14:27:53.169Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
